98875c4d24e758c64192d43655291322b895ab61
beckynevin <beckynevin@gmail.com>
2023-10-11 16:05:24 -0600

    removing calc_error_prop from utils

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -92,17 +92,6 @@ def sampling_summary_table(mcmc):
     print(f'divergences: {inf_data.sample_stats.diverging.values.sum()}')
     return az.summary(inf_data), inf_data
 
-def calc_error_prop(true_L, true_theta, true_a, dthing, time = 0.5, wrt = 'theta_0'):
-    if wrt == 'theta_0':
-        dx_dthing = true_L * np.cos(true_theta * np.cos(np.sqrt(true_a / true_L) * time)) * \
-              np.cos(np.sqrt(true_a / true_L) * time) * dthing
-    if wrt == 'L':
-
-        dx_dthing = (0.5 * true_theta * time * np.sqrt(true_a / true_L) * np.sin(time * np.sqrt(true_a / true_L)) * \
-             np.cos(true_theta * np.cos(time * np.sqrt(true_a / true_L))) + \
-             np.sin(true_theta * np.cos(time * np.sqrt(true_a / true_L)))) * dthing
-    return dx_dthing
-
 ## plotting and analysis utilities
 
 def display_pendulum_data(df):
