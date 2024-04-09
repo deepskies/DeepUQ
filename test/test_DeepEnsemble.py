4f0dae08824b6ca64a3ec31939de5d2de86ef72a
beckynevin <beckynevin@gmail.com>
2024-04-09 15:32:30 -0600

    should be pointing to correct test

--- test/test_DeepEnsemble.py
+++ test/test_DeepEnsemble.py
@@ -4,12 +4,6 @@ import os
 import subprocess
 import tempfile
 import shutil
-
-# flake8: noqa
-sys.path.append("..")
-# print(sys.path)
-# from scripts.evaluate import Diagnose_static, Diagnose_generative
-# from scripts.io import ModelLoader
 from scripts import evaluate, models, DeepEnsemble
 
 
@@ -35,58 +29,6 @@ def temp_directory():
     """
     shutil.rmtree(temp_dir)
 
-
-@pytest.mark.xfail(strict=True)
-def test_no_chkpt_saved_xfail(temp_directory):
-    noise_level = "low"
-    n_models = 10
-    wd = str(temp_directory) + "/"
-    n_epochs = 2
-    subprocess_args = [
-        "python",
-        "../src/scripts/DeepEnsemble.py",
-        noise_level,
-        str(n_models),
-        wd,
-        "--n_epochs",
-        str(n_epochs),
-    ]
-    # now run the subprocess
-    subprocess.run(subprocess_args, check=True)
-    # check if the right number of checkpoints are saved
-    models_folder = os.path.join(temp_directory, "models")
-    # list all files in the "models" folder
-    files_in_models_folder = os.listdir(models_folder)
-    # assert that the number of files is equal to 10
-    assert (
-        len(files_in_models_folder) == n_models
-    ), "Expected 10 files in the 'models' folder"
-
-
-def test_no_chkpt_saved(temp_directory):
-    noise_level = "low"
-    n_models = 10
-    wd = str(temp_directory) + "/"
-    n_epochs = 2
-    subprocess_args = [
-        "python",
-        "../src/scripts/DeepEnsemble.py",
-        noise_level,
-        str(n_models),
-        wd,
-        "--n_epochs",
-        str(n_epochs),
-    ]
-    # now run the subprocess
-    subprocess.run(subprocess_args, check=True)
-    # check if the right number of checkpoints are saved
-    models_folder = os.path.join(temp_directory, "models")
-    # list all files in the "models" folder
-    files_in_models_folder = os.listdir(models_folder)
-    # assert that the number of files is equal to 10
-    assert len(files_in_models_folder) == 0, "Expect 0 files in the 'models' folder"
-
-
 def test_chkpt_saved(temp_directory):
     noise_level = "low"
     n_models = 10
@@ -94,7 +36,7 @@ def test_chkpt_saved(temp_directory):
     n_epochs = 2
     subprocess_args = [
         "python",
-        "../src/scripts/DeepEnsemble.py",
+        "src/scripts/DeepEnsemble.py",
         noise_level,
         str(n_models),
         wd,
@@ -136,6 +78,59 @@ def test_chkpt_saved(temp_directory):
         ), f"File '{file_name}' does not contain the expected substring"
 
 
+
+@pytest.mark.xfail(strict=True)
+def test_no_chkpt_saved_xfail(temp_directory):
+    noise_level = "low"
+    n_models = 10
+    wd = str(temp_directory) + "/"
+    n_epochs = 2
+    subprocess_args = [
+        "python",
+        "src/scripts/DeepEnsemble.py",
+        noise_level,
+        str(n_models),
+        wd,
+        "--n_epochs",
+        str(n_epochs),
+    ]
+    # now run the subprocess
+    subprocess.run(subprocess_args, check=True)
+    # check if the right number of checkpoints are saved
+    models_folder = os.path.join(temp_directory, "models")
+    # list all files in the "models" folder
+    files_in_models_folder = os.listdir(models_folder)
+    # assert that the number of files is equal to 10
+    assert (
+        len(files_in_models_folder) == n_models
+    ), "Expected 10 files in the 'models' folder"
+
+
+def test_no_chkpt_saved(temp_directory):
+    noise_level = "low"
+    n_models = 10
+    wd = str(temp_directory) + "/"
+    n_epochs = 2
+    subprocess_args = [
+        "python",
+        "src/scripts/DeepEnsemble.py",
+        noise_level,
+        str(n_models),
+        wd,
+        "--n_epochs",
+        str(n_epochs),
+    ]
+    # now run the subprocess
+    subprocess.run(subprocess_args, check=True)
+    # check if the right number of checkpoints are saved
+    models_folder = os.path.join(temp_directory, "models")
+    # list all files in the "models" folder
+    files_in_models_folder = os.listdir(models_folder)
+    # assert that the number of files is equal to 10
+    assert len(files_in_models_folder) == 0, "Expect 0 files in the 'models' folder"
+
+
+
 def test_run_simple_ensemble(temp_directory):
     noise_level = "low"
     n_models = "10"
@@ -145,7 +140,7 @@ def test_run_simple_ensemble(temp_directory):
     wd = str(temp_directory) + "/"
     subprocess_args = [
         "python",
-        "../src/scripts/DeepEnsemble.py",
+        "src/scripts/DeepEnsemble.py",
         noise_level,
         n_models,
         wd,
@@ -162,7 +157,7 @@ def test_missing_req_arg(temp_directory):
     n_models = "10"
     subprocess_args = [
         "python",
-        "../src/scripts/DeepEnsemble.py",
+        "src/scripts/DeepEnsemble.py",
         noise_level,
         n_models,
         "--n_epochs",
