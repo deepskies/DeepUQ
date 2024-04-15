a1e2c89669bfcbff54e2029f86bcce0998d1ef4c
beckynevin <beckynevin@gmail.com>
2024-04-15 14:45:29 -0500

    removing model stuff from data.py

--- src/data/data.py
+++ src/data/data.py
@@ -65,42 +65,6 @@ def parse_args():
     return parser.parse_args()
 
 
-class ModelLoader:
-    def save_model_pkl(self, path, model_name, posterior):
-        """
-        Save the pkl'ed saved posterior model
-
-        :param path: Location to save the model
-        :param model_name: Name of the model
-        :param posterior: Model object to be saved
-        """
-        file_name = path + model_name + ".pkl"
-        with open(file_name, "wb") as file:
-            pickle.dump(posterior, file)
-
-    def load_model_pkl(self, path, model_name):
-        """
-        Load the pkl'ed saved posterior model
-
-        :param path: Location to load the model from
-        :param model_name: Name of the model
-        :return: Loaded model object that can be used with the predict function
-        """
-        print(path)
-        with open(path + model_name + ".pkl", "rb") as file:
-            posterior = pickle.load(file)
-        return posterior
-
-    def predict(input, model):
-        """
-
-        :param input: loaded object used for inference
-        :param model: loaded model
-        :return: Prediction
-        """
-        return 0
-
-
 class DataLoader:
     def save_data_pkl(self, data_name, data, path="../data/"):
         """
