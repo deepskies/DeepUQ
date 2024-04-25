1277e13d421c8f7958b05668ad5cc7b8b9e725c7
beckynevin <beckynevin@gmail.com>
2024-04-24 23:09:41 -0600

    passing most of DE tests

--- test/test_DeepEnsemble.py
+++ test/test_DeepEnsemble.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import tempfile
 import shutil
+import yaml
 
 
 @pytest.fixture
@@ -11,7 +12,10 @@ def temp_directory():
     temp_dir = tempfile.mkdtemp()
 
     # Create subdirectories within the temporary directory
-    models_dir = os.path.join(temp_dir, "models")
+    yaml_dir = os.path.join(temp_dir, "yamls")
+    os.makedirs(yaml_dir)
+
+    models_dir = os.path.join(temp_dir, "checkpoints")
     os.makedirs(models_dir)
 
     animations_dir = os.path.join(temp_dir, "images", "animations")
@@ -28,17 +32,97 @@ def temp_directory():
     shutil.rmtree(temp_dir)
 
 
+def create_test_config(temp_directory, n_epochs):
+    print('dumping temp yaml')
+    print('temp_dir', temp_directory)
+    input_yaml = {
+        "common": {"out_dir": str(temp_directory)},
+        "model": {"model_engine": "DE",
+                  "model_type": "DE",
+                  "loss_type": "bnll_loss",
+                  "init_lr": 0.001,
+                  "BETA": 0.5,
+                  "n_models": 2,
+                  "n_epochs": n_epochs,
+                  "save_all_checkpoints": False,
+                  "save_final_checkpoint": True,
+                  "overwrite_final_checkpoint": True,
+                  "plot": False,
+                  "savefig": True,
+                  "verbose": False,
+                    },
+        "data": {"data_path": "./data",
+                    "data_engine": "DataLoader",
+                    "size_df": 1000,
+                    "noise_level": "low",
+                    "val_proportion": 0.1,
+                    "randomseed": 42,
+                    "batchsize": 100,
+                    },
+        }
+    print('theoretically dumping here', str(temp_directory)+"yamls/DE.yaml")
+    yaml.dump(input_yaml, open(str(temp_directory)+"yamls/DE.yaml", "w"))
+
+
+def test_DE_from_config(temp_directory):
+    # create the test config dynamically
+    # make the temporary config file
+    n_epochs = 2
+    n_models = 2
+    create_test_config(temp_directory+"/", n_epochs)
+    subprocess_args = [
+        "python",
+        "src/scripts/DeepEnsemble.py",
+        "--config",
+        str(temp_directory)+"/yamls/DE.yaml",
+    ]
+    # now run the subprocess
+    subprocess.run(subprocess_args, check=True)
+    # check if the right number of checkpoints are saved
+    models_folder = os.path.join(temp_directory, "checkpoints")
+    print('this is the checkpoints folder', models_folder)
+    # list all files in the "models" folder
+    files_in_models_folder = os.listdir(models_folder)
+    print('files in checkpoints folder', files_in_models_folder)
+    # assert that the number of files is equal to 10
+    assert (
+        len(files_in_models_folder) == n_models
+    ), f"Expected {n_models} file in the 'checkpoints' folder"
+
+    # check if the right number of images were saved
+    animations_folder = os.path.join(temp_directory, "images/animations")
+    files_in_animations_folder = os.listdir(animations_folder)
+    assert (
+        len(files_in_animations_folder) == n_models
+    ), f"Expected {n_models} file in the 'images/animations' folder"
+
+    # also check that all files in here have the same name elements
+    expected_substring = "epoch_" + str(n_epochs - 1)
+    for file_name in files_in_models_folder:
+        assert (
+            expected_substring in file_name
+        ), f"File '{file_name}' does not contain the expected substring"
+
+    # also check that all files in here have the same name elements
+    for file_name in files_in_animations_folder:
+        assert (
+            expected_substring in file_name
+        ), f"File '{file_name}' does not contain the expected substring"
+
+
 def test_DE_chkpt_saved(temp_directory):
     noise_level = "low"
-    n_models = 10
-    wd = str(temp_directory) + "/"
+    n_models = 2
     n_epochs = 2
     subprocess_args = [
         "python",
         "src/scripts/DeepEnsemble.py",
+        "--noise_level",
         noise_level,
+        "--n_models",
         str(n_models),
-        wd,
+        "--out_dir",
+        str(temp_directory) + '/',
         "--n_epochs",
         str(n_epochs),
         "--save_final_checkpoint",
@@ -48,13 +132,13 @@ def test_DE_chkpt_saved(temp_directory):
     # now run the subprocess
     subprocess.run(subprocess_args, check=True)
     # check if the right number of checkpoints are saved
-    models_folder = os.path.join(temp_directory, "models")
+    models_folder = os.path.join(temp_directory, "checkpoints")
     # list all files in the "models" folder
     files_in_models_folder = os.listdir(models_folder)
     # assert that the number of files is equal to 10
     assert (
         len(files_in_models_folder) == n_models
-    ), "Expected 10 files in the 'models' folder"
+    ), f"Expected {n_models} files in the 'checkpoints' folder"
 
     # check if the right number of images were saved
     animations_folder = os.path.join(temp_directory, "images/animations")
@@ -62,7 +146,7 @@ def test_DE_chkpt_saved(temp_directory):
     # assert that the number of files is equal to 10
     assert (
         len(files_in_animations_folder) == n_models
-    ), "Expected 10 files in the 'images/animations' folder"
+    ), f"Expected {n_models} files in the 'images/animations' folder"
 
     # also check that all files in here have the same name elements
     expected_substring = "epoch_" + str(n_epochs - 1)
@@ -81,18 +165,20 @@ def test_DE_chkpt_saved(temp_directory):
 @pytest.mark.xfail(strict=True)
 def test_DE_no_chkpt_saved_xfail(temp_directory):
     noise_level = "low"
-    n_models = 10
-    wd = str(temp_directory) + "/"
+    n_models = 2
     n_epochs = 2
     subprocess_args = [
         "python",
         "src/scripts/DeepEnsemble.py",
+        "--noise_level",
         noise_level,
+        "--n_models",
         str(n_models),
-        wd,
+        "--out_dir",
+        str(temp_directory) + '/',
         "--n_epochs",
         str(n_epochs),
-        "--generatedata"
+        "--generatedata",
     ]
     # now run the subprocess
     subprocess.run(subprocess_args, check=True)
@@ -103,28 +189,32 @@ def test_DE_no_chkpt_saved_xfail(temp_directory):
     # assert that the number of files is equal to 10
     assert (
         len(files_in_models_folder) == n_models
-    ), "Expected 10 files in the 'models' folder"
+    ), f"Expected {n_models} files in the 'models' folder"
 
 
 def test_DE_no_chkpt_saved(temp_directory):
     noise_level = "low"
-    n_models = 10
-    wd = str(temp_directory) + "/"
+    n_models = 2
     n_epochs = 2
     subprocess_args = [
         "python",
         "src/scripts/DeepEnsemble.py",
+        "--noise_level",
         noise_level,
+        "--n_models",
         str(n_models),
-        wd,
+        "--out_dir",
+        str(temp_directory) + '/',
         "--n_epochs",
         str(n_epochs),
-        "--generatedata"
+        "--generatedata",
+        "--save_final_checkpoint",
+        "False"
     ]
     # now run the subprocess
     subprocess.run(subprocess_args, check=True)
     # check if the right number of checkpoints are saved
-    models_folder = os.path.join(temp_directory, "models")
+    models_folder = os.path.join(temp_directory, "checkpoints")
     # list all files in the "models" folder
     files_in_models_folder = os.listdir(models_folder)
     # assert that the number of files is equal to 10
@@ -134,17 +224,16 @@ def test_DE_no_chkpt_saved(temp_directory):
 
 def test_DE_run_simple_ensemble(temp_directory):
     noise_level = "low"
-    n_models = "10"
-    # here = os.getcwd()
-    # wd = self.temp_path
-    # os.path.dirname(here) + str(temp_directory) + '/'
-    wd = str(temp_directory) + "/"
+    n_models = 2
     subprocess_args = [
         "python",
         "src/scripts/DeepEnsemble.py",
+        "--noise_level",
         noise_level,
-        n_models,
-        wd,
+        "--n_models",
+        str(n_models),
+        "--out_dir",
+        str(temp_directory) + '/',
         "--n_epochs",
         "2",
         "--generatedata"
@@ -152,19 +241,3 @@ def test_DE_run_simple_ensemble(temp_directory):
     # now run the subprocess
     subprocess.run(subprocess_args, check=True)
 
-
-@pytest.mark.xfail(strict=True)
-def test_DE_missing_req_arg(temp_directory):
-    noise_level = "low"
-    n_models = "10"
-    subprocess_args = [
-        "python",
-        "src/scripts/DeepEnsemble.py",
-        noise_level,
-        n_models,
-        "--n_epochs",
-        "2",
-        "--generatedata"
-    ]
-    # now run the subprocess
-    subprocess.run(subprocess_args, check=True)
