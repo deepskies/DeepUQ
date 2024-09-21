f0eb23b22ce13c6f8746d619c675cad063eb8e78
beckynevin <beckynevin@gmail.com>
2024-09-20 20:33:01 -0700

    black and docstrings added to testing files

--- test/test_DeepEnsemble.py
+++ test/test_DeepEnsemble.py
@@ -7,10 +7,32 @@ import yaml
 from data.data import MyDataLoader, DataPreparation
 
 
-# @pytest.fixture(params=["low", "medium", "high", "vhigh"],
-#                name=["noise_level"])
 @pytest.fixture()
 def temp_data():  # noise_level, size_df):
+    """Create a temporary directory, generate synthetic data, and save it to an HDF5 file for testing purposes.
+
+    This pytest fixture creates a temporary directory with subdirectories for storing data. It uses the
+    `DataPreparation` class to sample parameters and simulate data with different noise levels. The simulated
+    data is saved as an HDF5 file in the temporary directory. After the test runs, the directory and its contents
+    are deleted.
+
+    Setup:
+        - Create a temporary directory and a 'data' subdirectory.
+        - Generate synthetic data using the `DataPreparation` class with specified parameters.
+        - Simulate data based on a noise level (`low`, `medium`, `high`, `vhigh`) which affects the value of `sigma`.
+        - Save the generated data as an HDF5 file in the temporary directory.
+
+    Teardown:
+        - Delete the temporary directory and all of its contents after the test completes.
+
+    Yields:
+        str: The path to the temporary directory containing the saved HDF5 file.
+
+    Example:
+        def test_example(temp_data):
+            data_dir = temp_data
+            # Use the data directory for testing
+    """
     # setup: Create a temporary directory with one folder level
     temp_dir = tempfile.mkdtemp()
 
@@ -56,6 +78,31 @@ def temp_data():  # noise_level, size_df):
 
 @pytest.fixture
 def temp_directory():
+    """Create a temporary directory with multiple subdirectories for testing purposes.
+
+    This pytest fixture sets up a temporary directory structure with subdirectories for storing YAML files,
+    model checkpoints, and image animations. The fixture yields the path to the root temporary directory,
+    which can be used in test functions. After the test completes, the entire directory and its contents
+    are deleted to ensure no leftover files remain.
+
+    Setup:
+        - Create a temporary root directory.
+        - Create subdirectories:
+            - 'yamls' for YAML files.
+            - 'checkpoints' for model checkpoints.
+            - 'images/animations' for animation image files.
+
+    Teardown:
+        - Delete the temporary root directory and all of its subdirectories and contents after the test completes.
+
+    Yields:
+        str: The path to the root temporary directory containing the subdirectories.
+
+    Example:
+        def test_example(temp_directory):
+            temp_dir = temp_directory
+            # Use the temp_dir for testing
+    """
     # setup: Create a temporary directory with one folder level
     temp_dir_root = tempfile.mkdtemp()
 
@@ -79,6 +126,28 @@ def temp_directory():
 def create_test_config(
     temp_directory, temp_data, n_epochs, noise_level="low", size_df=10
 ):
+    """Generate and save a test configuration YAML file for a deep ensemble model.
+
+    This function creates a dictionary containing configuration settings for a deep ensemble (DE) model,
+    including model parameters, data paths, and training settings. The configuration is based on the
+    specified number of epochs, data size, and noise level. It saves the configuration as a YAML file
+    in a temporary directory provided by the `temp_directory` fixture.
+
+    Args:
+        temp_directory (str): Path to the root temporary directory where the YAML file will be saved.
+        temp_data (str): Path to the directory containing the generated data for the test.
+        n_epochs (int): Number of epochs for model training.
+        noise_level (str, optional): Noise level for data generation, default is "low". Options include
+                                     "low", "medium", "high", and "vhigh".
+        size_df (int, optional): Size of the dataset used for testing, default is 10.
+
+    File Output:
+        Saves a YAML configuration file named `DE.yaml` in the 'yamls' subdirectory of the temporary directory.
+
+    Example:
+        create_test_config(temp_directory, temp_data, n_epochs=50, noise_level="medium", size_df=100)
+
+    """
     input_yaml = {
         "common": {"out_dir": str(temp_directory)},
         "model": {
@@ -123,16 +192,26 @@ def create_test_config(
 
 
 class TestDE:
-    # @pytest.mark.parametrize("noise_level, size_df",
-    #                        [(noise_level, size_df)])
-    # @pytest.mark.parametrize("size_df", [size_df])
-    # Add more values as needed
+    """
+    Test suite for validating the behavior of the Deep Ensemble (DE) model training process,
+    including checkpoint and image generation during training.
+    """
 
     def test_DE_from_config(
         self, temp_directory, temp_data, noise_level="low", size_df=10
     ):
-        # create the test config dynamically
-        # make the temporary config file
+        """Test the Deep Ensemble (DE) model using a dynamically created configuration file.
+
+        This test creates a YAML configuration file for the DE model, runs the training process
+        as a subprocess, and verifies that the correct number of checkpoints and animations
+        are saved during the training.
+
+        Args:
+            temp_directory (str): Path to the temporary directory where outputs are saved.
+            temp_data (str): Path to the generated data for testing.
+            noise_level (str, optional): Noise level for data generation, default is "low".
+            size_df (int, optional): Size of the dataset used for testing, default is 10.
+        """
         n_epochs = 2
         n_models = 2
         create_test_config(temp_directory + "/", temp_data, n_epochs)
@@ -178,6 +257,17 @@ class TestDE:
     def test_DE_chkpt_saved(
         self, temp_directory, temp_data, noise_level="low", size_df=10
     ):
+        """Test that the correct number of checkpoints and animations are saved during DE model training.
+
+        Runs the DE model training as a subprocess and verifies that checkpoints and animations
+        corresponding to each epoch are saved in the appropriate folders.
+
+        Args:
+            temp_directory (str): Path to the temporary directory where outputs are saved.
+            temp_data (str): Path to the generated data for testing.
+            noise_level (str, optional): Noise level for data generation, default is "low".
+            size_df (int, optional): Size of the dataset used for testing, default is 10.
+        """
         n_models = 2
         n_epochs = 2
         subprocess_args = [
@@ -236,6 +326,17 @@ class TestDE:
     def test_DE_no_chkpt_saved_xfail(
         self, temp_directory, temp_data, noise_level="low", size_df=10
     ):
+        """Test that expects failure when no checkpoints are saved.
+
+        This test simulates a scenario where checkpoints are expected but not saved, and the
+        test is expected to fail. The failure condition is checked using pytest's xfail marker.
+
+        Args:
+            temp_directory (str): Path to the temporary directory where outputs are saved.
+            temp_data (str): Path to the generated data for testing.
+            noise_level (str, optional): Noise level for data generation, default is "low".
+            size_df (int, optional): Size of the dataset used for testing, default is 10.
+        """
         n_models = 2
         n_epochs = 2
         subprocess_args = [
@@ -269,6 +370,17 @@ class TestDE:
     def test_DE_no_chkpt_saved(
         self, temp_directory, temp_data, noise_level="low", size_df=10
     ):
+        """Test that verifies no checkpoints are saved when expected.
+
+        This test runs the DE model training without saving any checkpoints and checks
+        that no files are present in the checkpoints folder.
+
+        Args:
+            temp_directory (str): Path to the temporary directory where outputs are saved.
+            temp_data (str): Path to the generated data for testing.
+            noise_level (str, optional): Noise level for data generation, default is "low".
+            size_df (int, optional): Size of the dataset used for testing, default is 10.
+        """
         n_models = 2
         n_epochs = 2
         subprocess_args = [
@@ -302,6 +414,17 @@ class TestDE:
     def test_DE_run_simple_ensemble(
         self, temp_directory, temp_data, noise_level="low", size_df=10
     ):
+        """Test the execution of a simple ensemble model with a few epochs.
+
+        This test runs the DE model training for 2 epochs as a subprocess to verify
+        that the ensemble model can run without errors.
+
+        Args:
+            temp_directory (str): Path to the temporary directory where outputs are saved.
+            temp_data (str): Path to the generated data for testing.
+            noise_level (str, optional): Noise level for data generation, default is "low".
+            size_df (int, optional): Size of the dataset used for testing, default is 10.
+        """
         n_models = 2
         subprocess_args = [
             "python",
