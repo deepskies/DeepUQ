b6d550a1e0d915cfba7f50b7245e7904df3419d9
beckynevin <beckynevin@gmail.com>
2024-09-21 08:56:28 -0700

    removing linear homoskedastic inject to output and input

--- src/data/data.py
+++ src/data/data.py
@@ -13,31 +13,6 @@ class MyDataLoader:
     def __init__(self):
         self.data = None
 
-    def save_data_pkl(self, data_name, data, path="../data/"):
-        """
-        Save and load the pkl'ed training/test set
-
-        :param path: Location to save the model
-        :param model_name: Name of the model
-        :param posterior: Model object to be saved
-        """
-        file_name = path + data_name + ".pkl"
-        with open(file_name, "wb") as file:
-            pickle.dump(data, file)
-
-    def load_data_pkl(self, data_name, path="../data/"):
-        """
-        Load the pkl'ed saved posterior model
-
-        :param path: Location to load the model from
-        :param model_name: Name of the model
-        :return: Loaded model object that can be used with the predict function
-        """
-        print(path)
-        with open(path + data_name + ".pkl", "rb") as file:
-            data = pickle.load(file)
-        return data
-
     def save_data_h5(self, data_name, data, path="../data/"):
         """
         Save data to an h5 file.
@@ -71,34 +46,6 @@ class MyDataLoader:
 
 
 class DataPreparation:
-    """
-    A class for loading, preprocessing, and simulating datasets.
-
-    Parameters:
-    - file_path (str): The path to the dataset file.
-
-    Methods:
-    - load_data(): Load data from the specified file path.
-    - preprocess_data(): Preprocess the loaded data.
-    - simulate_data(simulation_name, num_samples=1000):
-      Simulate data based on the specified simulation.
-    - save_data(output_file='output_data.csv'): Save the current dataset to
-      a CSV file.
-    - get_data(): Retrieve the current dataset.
-
-    Example Usage:
-    ```
-    dataset_manager = DatasetPreparation('your_dataset.csv')
-    dataset_manager.load_data()
-    dataset_manager.preprocess_data()
-    dataset_manager.simulate_data('linear')
-    dataset_manager.save_data('simulated_data.csv')
-    simulated_data = dataset_manager.get_data()
-    ```
-
-    Note: Replace 'your_dataset.csv' with the actual dataset file path.
-    """
-
     def __init__(self):
         self.data = None
 
@@ -163,8 +110,7 @@ class DataPreparation:
         center_y=50,
         theta=0,
         noise_level=0.0,
-        simulation_name="linear_homoskedastic",
-        inject_type="predictive",
+        inject_type="output",
         seed=42,
     ):
         image = GalaxyObject(
@@ -183,7 +129,7 @@ class DataPreparation:
         params,
         sigma,
         image_size=32,
-        inject_type="predictive",
+        inject_type="output",
         rs=40,
     ):
         # set the random seed
@@ -201,12 +147,12 @@ class DataPreparation:
                 theta=params[i, 2],
                 noise_level=0,
             )
-            if inject_type == "predictive":
+            if inject_type == "output":
                 image_array[i, :, :] = image
                 total_brightness.append(
                     np.sum(image) + np.random.normal(loc=0, scale=sigma)
                 )
-            elif inject_type == "feature":
+            elif inject_type == "input":
                 noisy_image = image + np.random.normal(
                     loc=0, scale=sigma, size=(image_size, image_size)
                 )
@@ -221,89 +167,81 @@ class DataPreparation:
         self,
         thetas,
         sigma,
-        simulation_name="linear_homoskedastic",
         x=np.linspace(0, 10, 100),
-        inject_type="predictive",
+        inject_type="output",
         seed=42,
         vary_sigma=False,
         verbose=False,
     ):
-        if simulation_name == "linear_homoskedastic":
-            # convert to numpy array (if tensor):
-            thetas = np.atleast_2d(thetas)
-            n_sim = thetas.shape[0]
-            print("number of sims", n_sim)
-            # Check if the input has the correct shape
-            if thetas.shape[1] != 2:
-                raise ValueError(
-                    "Input tensor must have shape (n, 2) where n is \
-                        the number of parameter sets."
-                )
-
-            # Unpack the parameters
-            if thetas.shape[0] == 1:
-                # If there's only one set of parameters, extract them directly
-                m, b = thetas[0, 0], thetas[0, 1]
-            else:
-                # If there are multiple sets of parameters,
-                # extract them for each row
-                m, b = thetas[:, 0], thetas[:, 1]
-            rs = np.random.RandomState(seed)  # 2147483648)#
-            # I'm thinking sigma could actually be a function of x
-            # if we want to get fancy down the road
-            # Generate random noise (epsilon) based
-            # on a normal distribution with mean 0 and standard deviation sigma
-            if vary_sigma:
-                print("YES WERE VARYING SIGMA")
-                new_sig = self.get_sigma_m(sigma, m)
-                ε = rs.normal(loc=0, scale=new_sig, size=(len(x), n_sim))
-                scale = new_sig
-            else:
-                print("NO WERE NOT VARYING SIGMA")
-                ε = rs.normal(loc=0, scale=sigma, size=(len(x), n_sim))
-                scale = sigma
-            if verbose:
-                plt.clf()
-                plt.hist(scale)
-                plt.annotate(
-                    "mean = " + str(np.mean(scale)),
-                    xy=(0.02, 0.9),
-                    xycoords="axes fraction",
-                )
-                plt.title("scale param, injection " + str(inject_type))
-                plt.show()
-            # Initialize an empty array to store the results
-            # for each set of parameters
-            x_noisy = np.zeros((len(x), thetas.shape[0]))
-            y_noisy = np.zeros((len(x), thetas.shape[0]))
-            y = np.zeros((len(x), thetas.shape[0]))
-            for i in range(thetas.shape[0]):
-                m, b = thetas[i, 0], thetas[i, 1]
-                if inject_type == "predictive":
-                    y_noisy[:, i] = m * x + b + ε[:, i]
-                    y[:, i] = m * x + b
-                elif inject_type == "feature":
-                    # y_prime[:, i] = m * (x + ε[:, i]) + b
-                    y[:, i] = m * x + b
-                    x_noisy[:, i] = x + ε[:, i]
-
-        else:
-            print(
-                f"Error: Unknown simulation name '{simulation_name}'. \
-                    No data generated."
+        # convert to numpy array (if tensor):
+        thetas = np.atleast_2d(thetas)
+        n_sim = thetas.shape[0]
+        print("number of sims", n_sim)
+        # Check if the input has the correct shape
+        if thetas.shape[1] != 2:
+            raise ValueError(
+                "Input tensor must have shape (n, 2) where n is \
+                    the number of parameter sets."
             )
-            return
-        if inject_type == "predictive":
+
+        # Unpack the parameters
+        if thetas.shape[0] == 1:
+            # If there's only one set of parameters, extract them directly
+            m, b = thetas[0, 0], thetas[0, 1]
+        else:
+            # If there are multiple sets of parameters,
+            # extract them for each row
+            m, b = thetas[:, 0], thetas[:, 1]
+        rs = np.random.RandomState(seed)  # 2147483648)#
+        # I'm thinking sigma could actually be a function of x
+        # if we want to get fancy down the road
+        # Generate random noise (epsilon) based
+        # on a normal distribution with mean 0 and standard deviation sigma
+        if vary_sigma:
+            print("YES WERE VARYING SIGMA")
+            new_sig = self.get_sigma_m(sigma, m)
+            ε = rs.normal(loc=0, scale=new_sig, size=(len(x), n_sim))
+            scale = new_sig
+        else:
+            print("NO WERE NOT VARYING SIGMA")
+            ε = rs.normal(loc=0, scale=sigma, size=(len(x), n_sim))
+            scale = sigma
+        if verbose:
+            plt.clf()
+            plt.hist(scale)
+            plt.annotate(
+                "mean = " + str(np.mean(scale)),
+                xy=(0.02, 0.9),
+                xycoords="axes fraction",
+            )
+            plt.title("scale param, injection " + str(inject_type))
+            plt.show()
+        # Initialize an empty array to store the results
+        # for each set of parameters
+        x_noisy = np.zeros((len(x), thetas.shape[0]))
+        y_noisy = np.zeros((len(x), thetas.shape[0]))
+        y = np.zeros((len(x), thetas.shape[0]))
+        for i in range(thetas.shape[0]):
+            m, b = thetas[i, 0], thetas[i, 1]
+            if inject_type == "output":
+                y_noisy[:, i] = m * x + b + ε[:, i]
+                y[:, i] = m * x + b
+            elif inject_type == "input":
+                # y_prime[:, i] = m * (x + ε[:, i]) + b
+                y[:, i] = m * x + b
+                x_noisy[:, i] = x + ε[:, i]
+
+        if inject_type == "output":
             # self.input = x
             self.input = torch.Tensor(np.tile(x, thetas.shape[0]).T)
             self.output = torch.Tensor(y_noisy.T)
             self.output_err = ε[:, i].T
-        elif inject_type == "feature":
+        elif inject_type == "input":
             self.input = torch.Tensor(x_noisy.T)
             self.output = torch.Tensor(y.T)
             self.output_err = ε[:, i].T
         print(
-            f"{simulation_name} simulation data generated, \
+            f"Data generated, \
                 with noise injected type: {inject_type}."
         )
 
@@ -324,15 +262,6 @@ class DataPreparation:
         prior = rs.uniform(
             low=low_bounds, high=high_bounds, size=(n_samples, n_params)
         )
-        """
-        the prior way of doing this (lol)
-        #print(np.shape(prior), prior)
-        #prior = Uniform(low=low_bounds,
-        #                high=high_bounds,
-        #                seed=seed)
-        # not random_seed, rs, or seed
-        #params = prior.sample((n_samples,))
-        """
         self.params = prior
 
     def get_dict(self):
@@ -348,19 +277,7 @@ class DataPreparation:
         return self.data
 
     def get_sigma_m(self, noise, m):
-        """_summary_
-
-        Args:
-            noise (_type_): _description_
-            inject_type (str, optional): _description_.
-            Defaults to "predictive".
-            data_dimension (str, optional): _description_.
-            Defaults to "0D".
-
-        Returns:
-            _type_: the value of injected sigma, for the feature injection this
-            is sigma_x, for the predictive injection, this is sigma_y
-        """
+        
         if noise == "low":
             sigma = 0.01 / abs(m)
         elif noise == "medium":
@@ -369,21 +286,8 @@ class DataPreparation:
             sigma = 0.10 / abs(m)
         return sigma
 
-    def get_sigma(noise, inject_type="predictive", data_dimension="0D"):
-        """_summary_
-
-        Args:
-            noise (_type_): _description_
-            inject_type (str, optional): _description_.
-            Defaults to "predictive".
-            data_dimension (str, optional): _description_.
-            Defaults to "0D".
-
-        Returns:
-            _type_: the value of injected sigma, for the feature injection this
-            is sigma_x, for the predictive injection, this is sigma_y
-        """
-        if inject_type == "predictive":
+    def get_sigma(noise, inject_type="output", data_dimension="0D"):
+        if inject_type == "output":
             if noise == "low":
                 sigma = 0.01
             elif noise == "medium":
@@ -394,14 +298,7 @@ class DataPreparation:
                 sigma = 1.00
             else:
                 print("cannot find a match for this noise", noise)
-        # elif inject_type == "feature" and data_dimension == "0D":
-        #    if noise == "low":
-        #        sigma = 1 / 5
-        #    elif noise == "medium":
-        #        sigma = 5 / 5
-        #    elif noise == "high":
-        #        sigma = 10 / 5
-        elif inject_type == "feature" and data_dimension == "2D":
+        elif inject_type == "input" and data_dimension == "2D":
             if noise == "low":
                 sigma = 0.01 / 32
             elif noise == "medium":
@@ -411,6 +308,27 @@ class DataPreparation:
         return sigma
 
     def normalize(inputs, ys_array, norm=False):
+        """Normalize input and output arrays, with optional normalization based on min-max scaling.
+
+        This function optionally normalizes the input (`inputs`) and output (`ys_array`) data 
+        using min-max scaling, which rescales the data to the range [0, 1]. If normalization 
+        is applied (`norm=True`), the function also returns the normalization parameters 
+        (min and max values for both inputs and outputs).
+
+        Args:
+            inputs (array-like): The input data to be normalized, typically features.
+            ys_array (array-like): The output data to be normalized, typically labels.
+            norm (bool, optional): If True, normalization is applied. If False, the data is 
+                                returned unchanged. Default is False.
+
+        Returns:
+            tuple: A tuple containing three elements:
+                - model_inputs (array-like): The (normalized) input data.
+                - model_outputs (array-like): The (normalized) output data.
+                - normalization_params (dict or None): A dictionary with the min and max values 
+                                                    used for normalization if `norm=True`, 
+                                                    otherwise None.
+        """
         if norm:
             # normalize everything before it goes into a network
             inputmin = np.min(inputs)  # , axis=0)
@@ -435,6 +353,29 @@ class DataPreparation:
     def train_val_split(
         model_inputs, model_outputs, val_proportion=0.1, random_state=42
     ):
+        """Split model inputs and outputs into training and validation sets.
+
+        This function takes input data (`model_inputs`) and corresponding output data 
+        (`model_outputs`) and splits them into training and validation sets based on 
+        the specified validation proportion. It uses the `train_test_split` method 
+        from scikit-learn to perform the split and ensures reproducibility through 
+        a random state.
+
+        Args:
+            model_inputs (array-like): The input data for the model, typically features.
+            model_outputs (array-like): The corresponding output data for the model, typically labels.
+            val_proportion (float, optional): The proportion of data to be used for validation. 
+                                            Default is 0.1 (10%).
+            random_state (int, optional): Seed used by the random number generator for reproducibility. 
+                                        Default is 42.
+
+        Returns:
+            tuple: A tuple containing four elements:
+                - x_train: The training set of inputs.
+                - x_val: The validation set of inputs.
+                - y_train: The training set of outputs.
+                - y_val: The validation set of outputs.
+        """
         x_train, x_val, y_train, y_val = train_test_split(
             model_inputs,
             model_outputs,
