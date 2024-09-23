c46295d1c9815150ffa2663208690a074b6480a6
beckynevin <beckynevin@gmail.com>
2024-09-23 13:39:23 -0700

    correcting line length stuff for analyze

--- src/analyze/analyze.py
+++ src/analyze/analyze.py
@@ -4,27 +4,28 @@ import torch
 
 
 class AggregateCheckpoints:
-    """A class for loading and processing model checkpoints for deep ensemble (DE)
-    and deep evidential regression (DER) models.
+    """A class for loading and processing model checkpoints for deep ensemble
+    (DE) and deep evidential regression (DER) models.
 
     This class provides methods for loading model checkpoints and extracting
     validation metrics, including uncertainty metrics for DER models.
 
     Methods:
-        load_checkpoint(model_name, prescription, inject_type, data_dim, noise, epoch, device, path="models/",
-                        BETA=0.5, nmodel=1, COEFF=0.5, loss="SDER", load_rs_chk=False, rs=42,
+        load_checkpoint(model_name, prescription, inject_type, data_dim, noise,
+                        epoch, device, path="models/", BETA=0.5, nmodel=1,
+                        COEFF=0.5, loss="SDER", load_rs_chk=False, rs=42,
                         load_nh_chk=False, nh=64):
             Loads a PyTorch model checkpoint based on the specified model type
             (DE or DER) and configuration parameters. Supports customizations
             for random seeds, hidden layers, and loss type.
 
         ep_al_checkpoint_DE(checkpoint):
-            Extracts mean and variance validation metrics from a loaded DE model
-            checkpoint.
+            Extracts mean and variance validation metrics from a loaded DE
+            model checkpoint.
 
         ep_al_checkpoint_DER(checkpoint):
-            Extracts epistemic and aleatoric uncertainty metrics (mean and standard
-            deviation) from a loaded DER model checkpoint.
+            Extracts epistemic and aleatoric uncertainty metrics (mean and
+            standard deviation) from a loaded DER model checkpoint.
     """
 
     def load_checkpoint(
@@ -46,7 +47,9 @@ class AggregateCheckpoints:
         load_nh_chk=False,
         nh=64,
     ):
-        """Loads a PyTorch model checkpoint from a .pt file, constructing the file name based on model type (DE or DER) and other configuration parameters.
+        """Loads a PyTorch model checkpoint from a .pt file, constructing the
+        file name based on model type (DE or DER) and other configuration
+        parameters.
 
         Parameters:
             model_name (str): Name of the model to load (e.g., 'DER', 'DE').
@@ -56,18 +59,23 @@ class AggregateCheckpoints:
             noise (float): Level of noise applied to the model.
             epoch (int): The epoch number to load the checkpoint for.
             device (str): The device ('cuda' or 'cpu') to load the model onto.
-            path (str): Directory path to the model checkpoints (default: "models/").
+            path (str): Directory path to the model checkpoints
+            (default: "models/").
             BETA (float): Beta value for DE models (default: 0.5).
             nmodel (int): Number of models in the ensemble (default: 1).
             COEFF (float): Coefficient for the DER model (default: 0.5).
             loss (str): Type of loss used for DER models (e.g., 'SDER', 'DER').
-            load_rs_chk (bool): Flag to indicate if random seed checkpoint is used (default: False).
+            load_rs_chk (bool): Flag to indicate if random seed checkpoint is
+            used (default: False).
             rs (int): Random seed value if load_rs_chk is True (default: 42).
-            load_nh_chk (bool): Flag to indicate if a specific hidden layer configuration is used (default: False).
-            nh (int): Number of hidden units if load_nh_chk is True (default: 64).
+            load_nh_chk (bool): Flag to indicate if a specific hidden layer
+            configuration is used (default: False).
+            nh (int): Number of hidden units if load_nh_chk is True
+            (default: 64).
 
         Returns:
-            dict: The loaded checkpoint containing model weights and additional data.
+            dict: The loaded checkpoint containing model weights and
+            additional data.
         """
         print(model_name)
         if model_name[0:3] == "DER":
@@ -112,8 +120,8 @@ class AggregateCheckpoints:
         return mean_validation, var_validation
 
     def ep_al_checkpoint_DER(self, checkpoint):
-        """Extracts mean and variance validation metrics from a loaded DER model
-        checkpoint.
+        """Extracts mean and variance validation metrics from a loaded DER
+        model checkpoint.
 
         Parameters:
             checkpoint (dict): The loaded DER model checkpoint.
